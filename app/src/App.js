162a5500627168ab8c785fa043dbf2c4ad532c35
Cadel Watson <cadel.watson@geora.io>
2021-08-17 18:51:32 +1000

    Set up digital taka wallet

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle } from "@drizzle/store";
 import drizzleOptions from "./drizzleOptions";
-import MyComponent from "./MyComponent";
+import DigitalTakaComponent from "./MyComponent";
 import "./App.css";
 
 const drizzle = new Drizzle(drizzleOptions);
@@ -11,20 +11,23 @@ const App = () => {
   return (
     <DrizzleContext.Provider drizzle={drizzle}>
       <DrizzleContext.Consumer>
-        {drizzleContext => {
+        {(drizzleContext) => {
           const { drizzle, drizzleState, initialized } = drizzleContext;
 
           if (!initialized) {
-            return "Loading..."
+            return "Loading...";
           }
 
           return (
-            <MyComponent drizzle={drizzle} drizzleState={drizzleState} />
-          )
+            <DigitalTakaComponent
+              drizzle={drizzle}
+              drizzleState={drizzleState}
+            />
+          );
         }}
       </DrizzleContext.Consumer>
     </DrizzleContext.Provider>
   );
-}
+};
 
 export default App;
